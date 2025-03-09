630aed6026dfcab7bfdde8824f91f3ea94e6e8da
Pedro Godri <86849487+pedrogodri@users.noreply.github.com>
2025-03-08 21:07:20 -0300

    feat: projeto backend criado

---
+++ organizacash/src/main/java/com/br/organizacash/OrganizacashApplication.java
@@ -0,0 +1,13 @@
+package com.br.organizacash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizacashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizacashApplication.class, args);
+	}
+
+}
---
+++ organizacash/src/test/java/com/br/organizacash/OrganizacashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.organizacash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizacashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

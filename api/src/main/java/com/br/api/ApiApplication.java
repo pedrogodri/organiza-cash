ff66040142d4e854535148cd419ccc10fb135883
Pedro Godri <86849487+pedrogodri@users.noreply.github.com>
2025-03-08 21:21:38 -0300

    fix: alterando nome do projeto do backend

--- api/src/main/java/com/br/api/ApiApplication.java
+++ api/src/main/java/com/br/api/ApiApplication.java
@@ -1,13 +1,13 @@
-package com.br.organizacash;
+package com.br.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrganizacashApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrganizacashApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
--- api/src/test/java/com/br/api/ApiApplicationTests.java
+++ api/src/test/java/com/br/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.br.organizacash;
+package com.br.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrganizacashApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
